feaa7ee90cc86bceadc4c8037103bace7c4b743f
System Administrator <root@MacBook-Air-di-Alessandro.local>
2022-12-28 11:18:43 +0100

    implementazione service Ordine

---
+++ src/main/java/it/prova/pizzastore/service/ordine/OrdineService.java
@@ -0,0 +1,21 @@
+package it.prova.pizzastore.service.ordine;
+
+import java.util.List;
+
+import it.prova.pizzastore.model.Ordine;
+
+public interface OrdineService {
+	
+	public List<Ordine> listAll();
+	
+	public Ordine caricaSingoloOrdine(Long id);
+	
+	public void aggiorna (Ordine ordineInstance);
+	
+	public void inserisciNuovo(Ordine ordineInstance);
+	
+	public void elimina(Long idRemove);
+	
+	public List<Ordine> findByExample(Ordine example);
+
+}
---
+++ src/main/java/it/prova/pizzastore/service/ordine/OrdioneServiceImpl.java
@@ -0,0 +1,49 @@
+package it.prova.pizzastore.service.ordine;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import it.prova.pizzastore.model.Ordine;
+import it.prova.pizzastore.repository.ordine.OrdineRepository;
+
+public class OrdioneServiceImpl implements OrdineService{
+	
+	@Autowired
+	private OrdineRepository ordineRepository;
+
+	@Override
+	public List<Ordine> listAll() {
+		return (List<Ordine>) ordineRepository.findAll();
+	}
+
+	@Override
+	public Ordine caricaSingoloOrdine(Long id) {
+		return ordineRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public void aggiorna(Ordine ordineInstance) {
+		 ordineRepository.save(ordineInstance);
+		
+	}
+
+	@Override
+	public void inserisciNuovo(Ordine ordineInstance) {
+		ordineRepository.save(ordineInstance);
+		
+	}
+
+	@Override
+	public void elimina(Long idRemove) {
+		ordineRepository.deleteById(idRemove);
+		
+	}
+
+	@Override
+	public List<Ordine> findByExample(Ordine example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
